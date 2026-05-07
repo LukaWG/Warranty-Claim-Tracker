3f93ccdfceb5941379ff9c63db9e0b0a52647d82
Luka Wilson-Green <lwilson-green@hendy-group.com>
2026-05-07 08:58:29 +0100

    Remove supabase dependency to fix GitHub Action

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,19 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export async function getData(tableName, selectQuery = '*') {
-  const { data, error } = await supabase
-    .from(tableName)
-    .select(selectQuery);
+// export async function getData(tableName, selectQuery = '*') {
+//   const { data, error } = await supabase
+//     .from(tableName)
+//     .select(selectQuery);
 
-  if (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
+//   if (error) {
+//     console.error('Error fetching data:', error);
+//     throw error;
+//   }
 
-  return data;
-}
+//   return data;
+// }
