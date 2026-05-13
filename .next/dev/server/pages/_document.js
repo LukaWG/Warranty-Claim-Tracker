a8f8c2959404d3439e6cf5ebf8a0f2da34120125
Luka Wilson-Green <lukawg@icloud.com>
2026-05-13 15:34:07 +0100

    Ignore .next

--- .next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/_0avdysa._.js")
-R.c("server/chunks/ssr/[externals]__12~~src._.js")
-R.m("[project]/Desktop/Dev/Hendy/Warranty-Claim-Tracker/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/Dev/Hendy/Warranty-Claim-Tracker/node_modules/next/document.js [ssr] (ecmascript)").exports
