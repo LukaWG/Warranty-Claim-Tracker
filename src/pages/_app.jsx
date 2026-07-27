bd5ec001b96dee98bdd0cbdccc649a873c61ed28
Luka Wilson-Green <lwilson-green@hendy-group.com>
2026-07-27 14:59:56 +0100

    Remove uncessary _app wrappers

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,8 +5,6 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientInstance } from '@/lib/query-client';
-import VisualEditAgent from '@/lib/VisualEditAgent';
-import NavigationTracker from '@/lib/NavigationTracker';
 import { Toaster } from '@/components/ui/toaster';
 import { pagesConfig } from '@/pages.config';
 
