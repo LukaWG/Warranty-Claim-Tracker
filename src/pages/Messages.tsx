0f0643beb6a0fb97ff54866ae56daa36e84a9fc4
Luka Wilson-Green <lwilson-green@hendy-group.com>
2026-08-06 12:50:43 +0100

    Widen message modal

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -293,7 +293,7 @@ export default function Messages() {
       {/* Thread Dialog */}
       {selectedThread && (
         <Dialog open={!!selectedThread} onOpenChange={() => setSelectedThread(null)}>
-          <DialogContent className="max-w-lg">
+          <DialogContent className="max-w-2x1">
             <MessageThread
               rootMessage={selectedThread}
               replies={getReplies(selectedThread.id)}
